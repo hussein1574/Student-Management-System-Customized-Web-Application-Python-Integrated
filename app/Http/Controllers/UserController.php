e153710ec9cb4062406b9bd8122d34ebacb68324
hussein1574 <medhathussein2@gmail.com>
2023-07-06 20:03:14 +0300

    put the image url instead of name in the json

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,42 +11,66 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-     public function changePassword(Request $request)
+    public function changePassword(Request $request)
     {
         //hash the current password and compare it with the password in the database
-        if(!Hash::check($request->current_password, Auth::user()->password)){
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'Current password is incorrect',
-            ], 401);
+        if (!Hash::check($request->current_password, Auth::user()->password)) {
+            return response()->json(
+                [
+                    "status" => "failed",
+                    "message" => "Current password is incorrect",
+                ],
+                401
+            );
         }
-        if(strcmp($request->current_password, $request->password) == 0){
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'New password cannot be same as your current password',
-            ], 401);
+        if (strcmp($request->current_password, $request->password) == 0) {
+            return response()->json(
+                [
+                    "status" => "failed",
+                    "message" =>
+                        "New password cannot be same as your current password",
+                ],
+                401
+            );
         }
-        if($request->password != $request->password_confirmation){
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'New password and confirm password must be same',
-            ], 401);
+        if ($request->password != $request->password_confirmation) {
+            return response()->json(
+                [
+                    "status" => "failed",
+                    "message" =>
+                        "New password and confirm password must be same",
+                ],
+                401
+            );
         }
         $request->user()->password = $request->password;
         $request->user()->save();
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Password updated successfully',
-        ], 200);
+        return response()->json(
+            [
+                "status" => "success",
+                "message" => "Password updated successfully",
+            ],
+            200
+        );
     }
 
     public function profile(Request $request)
     {
-        return response()->json([
-            'status' => 'success',
-            'user' => $request->user(),
-        ], 200);
+        $user = $request->user();
+        $profilePicFileName = $user->profilePicture;
+        // get the profile picture from the storage as a url
+        $profilePic = null;
+        if ($profilePicFileName != null) {
+            $profilePic = asset("storage/images/$profilePicFileName");
+        }
+        $user->profilePicture = $profilePic;
+        return response()->json(
+            [
+                "status" => "success",
+                "user" => $user,
+            ],
+            200
+        );
     }
-   
 }
